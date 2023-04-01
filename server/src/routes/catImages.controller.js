8291096979ad1362a1ac2b1108562e65c750e31d
Oki Kehinde <olamiposi.oki@gmail.com>
2023-04-01 23:30:02 +0100

    get 8 images

--- server/src/routes/catImages.controller.js
+++ server/src/routes/catImages.controller.js
@@ -4,16 +4,18 @@ require('dotenv').config();
 async function httpGetCatImagesByBreed(req, res) {
 	const breedId = req.body.breed_id;
 	const apiKey = process.env.API_KEY;
+	const limit = 8;
+
 	try {
 		const images = await axios.get(
-			`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`,
+			`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&limit=${limit}`,
 			{
 				headers: {
 					'x-api-key': apiKey,
 				},
 			},
 		);
-		return res.status(200).json(images.data);
+		return res.status(200).json(images.data.map((image) => image.url));
 	} catch (error) {
 		return res.send(error);
 	}
