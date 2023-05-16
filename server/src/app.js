90ae0f522c44c51a1a966df1106b887784ab8bc9
Oki Kehinde <olamiposi.oki@gmail.com>
2023-05-16 11:36:21 +0100

    enable cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
 
 const catBreedRouter = require('./routes/catBreeds.route');
 const topCatBreedsRouter = require('./routes/topCatBreed.router');
 const catImagesRouter = require('./routes/catImages.router');
 
+app.use(
+	cors({
+		origin: 'http://localhost:5173',
+	}),
+);
 app.use(express.json());
 
 // route
