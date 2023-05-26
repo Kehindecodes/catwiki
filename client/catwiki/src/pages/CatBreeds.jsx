323fa44af3a398bfa4c3aa76b659ca8e36544aa2
Oki Kehinde <olamiposi.oki@gmail.com>
2023-05-26 16:36:43 +0100

    make  topbreeds responsive

--- client/catwiki/src/pages/CatBreeds.jsx
+++ client/catwiki/src/pages/CatBreeds.jsx
@@ -5,13 +5,12 @@ import cat from '../assets/image 3.png';
 import { httpGetTopBreeds } from '../hooks/request';
 const CatBreeds = () => {
 	const [topBreeds, setTopBreeds] = useState([]);
+
 	useEffect(() => {
 		async function fetchData() {
 			try {
 				const topBreedsresponse = await httpGetTopBreeds();
-
 				const topBreeds = topBreedsresponse.data;
-
 				setTopBreeds(topBreeds);
 				console.log(topBreeds);
 			} catch (error) {
@@ -21,32 +20,33 @@ const CatBreeds = () => {
 
 		fetchData();
 	}, []);
+
 	return (
 		<>
 			<Header />
-			<div className='mx-16 mt-6 min-h-screen '>
-				<h1 className='text-primary font-bold text-4xl font-montserrat'>
+			<div className='mx-6 sm:mx-16 mt-6 min-h-screen'>
+				<h1 className='text-primary font-bold text-4xl sm:text-5xl font-montserrat'>
 					Top 10 most searched breeds
 				</h1>
-				<div className='flex  mt-12 flex-col'>
+				<div className='flex flex-col mt-12'>
 					{topBreeds.map((topbreed, index) => (
-						<div className='flex mb-8' key={index}>
+						<div
+							className='flex flex-col lg:flex-row mb-10 lg:mb-8'
+							key={index}>
 							{/* image */}
-							<div className='w-breed h-breed'>
+							<div className=' w-full lg:w-breed  h-image1 lg:h-breed '>
 								<img
 									src={topbreed.image.url}
 									alt='cat'
-									className='w-full h-full rounded-3xl'
+									className='w-full h-full rounded-3xl object-cover'
 								/>
 							</div>
 							{/* breed details */}
-							<div className='ml-10 w-4/5'>
-								<h3
-									className='font-montserrat font-semibold text-4xl text-primary
-                        '>
+							<div className='ml-0 lg:ml-10 w-full sm:w-catDetails mt-6 lg:mt-0'>
+								<h3 className='font-montserrat font-semibold text-2xl sm:text-4xl text-primary'>
 									<span>{index + 1}.</span> {topbreed.name}
 								</h3>
-								<p className='font-bold font-montserrat text-lg text-primary mt-5'>
+								<p className='font-bold font-montserrat text-base sm:text-lg text-primary mt-3 sm:mt-5'>
 									{topbreed.description}
 								</p>
 							</div>
